fe302f5f95ae71285aa55b06b25237a46c33fd5a
RiriFang <62824730+RiRiFangRui@users.noreply.github.com>
2020-04-01 21:58:05 +0800

    Update and rename for4.py to 素数.py

--- Day01-15/code/Day04/素数.py
+++ Day01-15/code/Day04/素数.py
@@ -18,3 +18,17 @@ if is_prime and num != 1:
     print('%d是素数' % num)
 else:
     print('%d不是素数' % num)
+
+    
+a = int(input('please enter number here : '))
+count = 0
+i = 1
+for i in range(1, a+1):
+    if a % i == 0:
+        count = count + 1
+if count == 2 and a > 0:
+    print('is sushu')
+else:
+    print('is not sushu')
+    
+    总结：随缘吧
