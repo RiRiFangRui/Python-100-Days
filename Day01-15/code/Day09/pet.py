6a300337c32b0ecdabade4dafffb240132b0b0eb
RiriFang <62824730+RiRiFangRui@users.noreply.github.com>
2020-04-10 15:01:02 +0800

    Update pet.py

--- Day01-15/code/Day09/pet.py
+++ Day01-15/code/Day09/pet.py
@@ -34,4 +34,5 @@ if __name__ == '__main__':
 
     
 总结：
-    虽然图文关系不太大，在学习进度中pet上面一道程序出现super()函数，https://blog.csdn.net/qq_38787214/article/details/87902291
+    虽然图文关系不太大，在学习进度中pet上面一道程序出现super()函数，可以调用父类属性。https://blog.csdn.net/qq_38787214/article/details/87902291
+    11行，pass，占位语句，执行程序时看到跳过。https://blog.csdn.net/violet_echo_0908/article/details/52052054
