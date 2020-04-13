0e08823f8140d99565b749e9a5415a4b40187bd3
RiriFang <62824730+RiRiFangRui@users.noreply.github.com>
2020-04-13 12:19:55 +0800

    Update file1.py

--- Day01-15/code/Day11/file1.py
+++ Day01-15/code/Day11/file1.py
@@ -29,3 +29,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    
+    
+    
+总结：
+
+【进行逻辑判断（比如if）时，Python当中等于False的值并不只有False一个，它也有一套规则。
+对于基本类型来说，基本上每个类型都存在一个值会被判定为False。
+布尔型，False表示False，其他为True整数和浮点数，0表示False，其他为True
+字符串和类字符串类型（包括bytes和unicode），空字符串表示False，其他为True
+序列类型（包括tuple，list，dict，set等），空表示False，非空表示True
+None永远表示False
+
+作者：灵剑
+链接：https://www.zhihu.com/question/48707732/answer/112233903
+来源：知乎
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。】
