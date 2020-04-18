a95964b250df18771af55975f4ecf8b855f1ed0e
RiriFang <62824730+RiRiFangRui@users.noreply.github.com>
2020-04-18 19:31:31 +0800

    Update csv1.py

--- Day01-15/code/Day11/csv1.py
+++ Day01-15/code/Day11/csv1.py
@@ -19,3 +19,7 @@ except FileNotFoundError:
 else:
     for item in data:
         print('%-30s%-20s%-10s' % (item[0], item[1], item[2]))
+        
+        
+        
+总结：21行%-30是item[0] 一共占30个宽度，这样输出会很整齐。
